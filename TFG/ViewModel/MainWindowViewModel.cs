f03f67bc37a4b65c85526b4052fd3ecc1e6006b7
AaronAka <aaronmape@hotmail.com>
2023-05-21 19:47:13 +0200

    Basic doc reading and architecture. Created dummy files to upload the new folders

---
+++ TFG/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,92 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using Microsoft.Win32;
+using Microsoft.Office.Interop.Word;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.ComponentModel;
+
+namespace TFG.ViewModel
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private ICommand _openFileDialogCommand;
+        private string _fileContent = "";
+
+        public MainWindowViewModel() 
+        {
+            _openFileDialogCommand = new RelayCommand(ReadUserSelectedFile, ReadUserSelectedFileCanExecute);
+        }
+        
+        public ICommand OpenFileDialogCommand
+        {
+            get
+            {
+                return _openFileDialogCommand;
+            }
+            set
+            {
+                if (value != null)
+                {
+                    _openFileDialogCommand = value;
+                }
+            }
+        }
+
+        public string FileContent
+        {
+            get
+            {
+                return _fileContent;
+            }
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    _fileContent = value;
+                }
+                OnPropertyChanged("FileContent");
+            }
+        }
+
+        private bool ReadUserSelectedFileCanExecute()
+        {
+            return true;
+        }
+
+        private void ReadUserSelectedFile()
+        {
+            try
+            {
+                //TODO: Replace document opening logic with Aspose library to avoid visibly opening Word on the user's computer
+                OpenFileDialog fileDialog = new();
+                fileDialog.Filter = "Word documents (.doc; .docx)|*.doc;*.docx";
+                fileDialog.ShowDialog();
+
+                string selectedFilePath  = fileDialog.FileName.ToString();
+
+                if (!string.IsNullOrEmpty(selectedFilePath))
+                {
+                    Microsoft.Office.Interop.Word.Application app = new();
+                    Document doc = app.Documents.Open(selectedFilePath);
+                    FileContent = doc.Content.Text;
+                    app.Quit();
+                }
+            } 
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error has occurred while importing the file " + ex.ToString());
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
